a62ccca4c81bf605292068a2eaff6736178bf1df
stayhungry07212 <stay.hungry07212@gmail.com>
2025-05-06 11:14:49 +0300

    Create role check middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (auth()->check() && auth()->user()->role !== $role) {
+            return response()->json(['error' => 'Unauthorized. Requires ' . $role . ' role.'], 403);
+        }
+
+        return $next($request);
+    }
+}
